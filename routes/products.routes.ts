c7cf4fe3fdcee0d7328960f8610f01a9ca6b1dd6
potocki92 <mateusz.potocki92@gmail.com>
2023-11-15 23:04:07 +0100

    feat/products.routes.ts
    feat(products): implement Swagger documentation for createProduct endpoint
    
    - Add Swagger documentation for the POST /api/products endpoint
    - Include summary, description, parameters, and responses

--- routes/products.routes.ts
+++ routes/products.routes.ts
@@ -1,5 +1,8 @@
 import express from "express";
-import { getAllProducts } from "../controllers/products.controller";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/products.controller";
 
 const router = express.Router();
 
@@ -36,5 +39,44 @@ const router = express.Router();
  *                   example: Internal server error
  */
 router.get("/", getAllProducts);
-
+/**
+ * @swagger
+ * /api/products:
+ *   post:
+ *     summary: Create a new product
+ *     description: Creates a new product with the provided details.
+ *     tags:
+ *       - Products
+ *     parameters:
+ *       - in: body
+ *         name: products
+ *         description: Product to be created
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *               example: "Miód Rzepakowy"
+ *             price:
+ *               type: number
+ *               example: 45.00
+ *             description:
+ *               type: string
+ *               example: "Smaczny miód rzepakowy"
+ *     responses:
+ *       201:
+ *         description: Successfully created a new product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ProductBody'
+ *       400:
+ *         description: Bad Request - Missing required fields.
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Missing required fields: name"
+ */
+router.post("/", createProduct);
 export { router as productRouter };
