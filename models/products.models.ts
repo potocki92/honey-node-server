f7be055234ec8b080e8ce3561c21db7c5b60f827
potocki92 <mateusz.potocki92@gmail.com>
2023-11-23 11:15:59 +0100

    chore/products.models
    chore: simplify validation in product schema
    
    - Remove unnecessary 'validate' field from product schema for name and price

--- models/products.models.ts
+++ models/products.models.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-interface IProduct extends Document{
+interface IProduct extends Document {
   name: string;
   price: number;
   description?: string;
@@ -10,12 +10,6 @@ const productSchema: Schema = new Schema<IProduct>({
   name: {
     type: String,
     require: [true, "Nazwa produktu jest wymagana"],
-    validate: {
-      validator: (value: string) => {
-        return value.length > 5
-      },
-      message: "Nazwa produktu musi mieć co najmniej 6 znakow"
-    }
   },
   description: {
     type: String,
@@ -23,12 +17,6 @@ const productSchema: Schema = new Schema<IProduct>({
   price: {
     type: Number,
     require: [true, "Cena produktu jest wymagana"],
-    validate: {
-      validator: (value: number) => {
-        return value >= 0;
-      },
-      message: "Cena produktu nie może być ujemna",
-    },
   },
 });
 
