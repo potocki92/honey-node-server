2a98a7c9dc87bbf201a58fbebc7326e3f284456e
potocki92 <mateusz.potocki92@gmail.com>
2023-11-23 11:12:12 +0100

    feat/validation
    feat: add detailed validation schema for product data using Joi
    
    - Introduce a comprehensive validation schema for product data using Joi
    - Include specific error messages for each validation rule

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,14 +1,20 @@
 import Joi from "joi";
 
-export const productSchema = Joi.object({
+export const productValidation = Joi.object({
   name: Joi.string().required().messages({
     "string.base": "Pole 'name' powinno być typu tekstowego.",
-    "any.required": "Pole 'name' jest wymagane.",
+    "string.min": "Pole powinno zawierać przynajmniej 6 znaków.",
+    "string.max": "Pole powinno zawierać maksymalnie 20 znaków.",
+    "string.required": "Nazwa produktu jest wymagana.",
   }),
-  price: Joi.number().required().messages({
-    "number.base": "Pole 'price' powinno być liczbą.",
-    "number.required": "Pole 'price' jest wymagane.",
-    "number.type": "Pole 'price' musi być liczbą.",
-  }),
+  price: Joi.number()
+    .required()
+    .messages({
+      "number.base": "Pole 'price' powinno być liczbą.",
+      "number.type": "Pole 'price' musi być liczbą.",
+      "number.required": "Cena produktu jest wymagana.",
+      "number.positive": "Musisz podać wartość dodatnią",
+    })
+    .positive(),
   description: Joi.string(),
 });
